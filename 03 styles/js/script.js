c1380da4892e2ab436374fd5aa6d2bbd9b70f018
bitsoyeva <bitsoyeva1997@gmail.com>
2019-10-10 23:38:30 +0300

    4.2.1Modul

---
+++ 03 styles/js/script.js
@@ -0,0 +1,8 @@
+console.log('you re at '+window.location);
+
+const Colored = document.querySelector('.colored');
+console.log(Colored);
+console.log(Colored.style);
+let quest=prompt('Какой ты хочешь цвет?')
+//Colored.style.backgroundColor='blue';
+Colored.style['background-color']=quest;
---
+++ 4.2.1_Практика.js
@@ -0,0 +1,6 @@
+let myNumber=prompt('Введите число')
+if (myNumber>=0){
+console.log(myNumber)
+}else{
+console.log(Math.abs(myNumber))
+}
